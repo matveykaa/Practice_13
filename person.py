d9a263e3876858d6bfdf5a5b10d34e04a8849049
Aliona Matweyenko <matveenkoalena2844@mail.ru>
2023-07-17 23:24:03 +0300

    Edit person task

--- person.py
+++ person.py
@@ -3,12 +3,17 @@ class Person:
         self.name = name
         self.age = age
     def print_info(self):
-        print("Привет, меня зовут", self.name, "и меня зовут ", self.age)
+        print("Привет, меня зовут", self.name.capitalize(), "и мне ",self.age, "лет")
+
+    def can_vote(self):
+        if self.age >= '18':
+            print("Совершеннолетний")
 
 def person_func(input_info):
     name, age = input_info.split()
     person = Person(name, age)
     person.print_info()
+    person.can_vote()
 
 if __name__ == "__main__":
     input_info = input("Введите имя и возраст через пробел - ")
